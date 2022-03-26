c8b8c913ce9b6bfb9469e95b0c74524b4c147e10
Joshua Shannon <retrodaredevil@gmail.com>
2022-03-26 09:50:12 -0500

    Building opencv manually working, building vision working

--- build_scripts/lightning_install_vision.sh
+++ build_scripts/lightning_install_vision.sh
@@ -4,19 +4,30 @@ echo "Installing dependencies for vision install"
 
 # We assume that apt-get update has been run recently
 
-# ninja-build is needed for building opencv using pip install
-# python (python2) is needed for numpy, which gets installed when installing opencv-contrib-python-headless
-apt-get install -y build-essential cmake pkg-config g++ ninja-build git python3 python3-pip python3-dev python || exit 1
-python2 --version
+apt-get install -y build-essential cmake pkg-config git || exit 1
+apt-get install -y libjpeg-dev libtiff-dev libjasper-dev libpng-dev libwebp-dev libopenexr-dev || exit 1
+apt-get install -y libavcodec-dev libavformat-dev libswscale-dev libv4l-dev libxvidcore-dev libx264-dev libdc1394-22-dev libgstreamer-plugins-base1.0-dev libgstreamer1.0-dev || exit 1
+#apt-get install libgtk-3-dev libqtgui4 libqtwebkit4 libqt4-test python3-pyqt5 || exit 1
+apt-get install -y libhdf5-dev libhdf5-103 || exit 1
 
-# Thanks https://stackoverflow.com/a/53402396/5434860 - numpy needs this, and the opencv stuff needs numpy
-# In the future, we could just install python3-numpy using apt-get, but that's no fun
-#apt-get install -y libatlas-base-dev libhdf5-dev libhdf5-serial-dev libjasper-dev || exit 1
-apt-get install -y libatlas-base-dev libjasper-dev || exit 1
+cd /opt/ || exit 1
+git clone --single-branch --depth=1 https://github.com/opencv/opencv.git || exit 1
+cd opencv/ || exit 1
+mkdir build/ || exit 1
+cd build/ || exit 1
+cmake -D CMAKE_BUILD_TYPE=RELEASE \
+    -D CMAKE_INSTALL_PREFIX=/usr/local \
+    -D ENABLE_NEON=ON \
+    -D ENABLE_VFPV3=ON \
+    -D BUILD_TESTS=OFF \
+    -D INSTALL_PYTHON_EXAMPLES=OFF \
+    -D OPENCV_ENABLE_NONFREE=ON \
+    -D CMAKE_SHARED_LINKER_FLAGS=-latomic \
+    -D BUILD_EXAMPLES=OFF ..
 
-python3 -m pip install --upgrade pip setuptools wheel || exit 1  # wheel needs to be installed before we install opencv
-#python3 -m pip install opencv-contrib-python-headless || exit 1  # includes main modules and contrib/extra modules
-python3 -m pip install --only-binary :all: opencv-python-headless || exit 1  # includes main modules and contrib/extra modules
+make "-j$(nproc)" || exit 1
+make install || exit 1
+ldconfig || exit 1
 
 
 cd /opt/ || exit 1
